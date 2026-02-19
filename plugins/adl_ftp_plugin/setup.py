5234bcb3204142ee0a19ccef99072f93faade0ff
Erick Otenyo <otenyo.erick@gmail.com>
2026-02-19 14:37:03 +0300

    Bump version

--- plugins/adl_ftp_plugin/setup.py
+++ plugins/adl_ftp_plugin/setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 PROJECT_DIR = os.path.dirname(__file__)
 REQUIREMENTS_DIR = os.path.join(PROJECT_DIR, "requirements")
-VERSION = "0.8.2"
+VERSION = "0.8.3"
 
 
 def get_requirements(env):
