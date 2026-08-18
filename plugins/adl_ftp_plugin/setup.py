4d436f82594ef17f826c934780bad2ff14d385e3
Erick Otenyo <otenyo.erick@gmail.com>
2026-08-18 22:26:54 +0300

    chore: bump version to 0.12.0

--- plugins/adl_ftp_plugin/setup.py
+++ plugins/adl_ftp_plugin/setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 PROJECT_DIR = os.path.dirname(__file__)
 REQUIREMENTS_DIR = os.path.join(PROJECT_DIR, "requirements")
-VERSION = "0.11.2"
+VERSION = "0.12.0"
 
 
 def get_requirements(env):
